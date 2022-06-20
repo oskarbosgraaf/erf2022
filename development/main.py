c49bf47bb1d65846438bb69e841ad11cd5848be9
dweprinz <73822624+dweprinz@users.noreply.github.com>
2022-06-20 16:12:17 +0200

    Update main.py

--- development/main.py
+++ development/main.py
@@ -1,26 +1,69 @@
+import numpy as np
 # Current code for the main program
 
 class Arbiter:
     def __init__(self, lidar, distance_front, distance_left):
-        self.behavior_names = ['follow wall', 'find wall', 'avoid', 'homing', 'corridor']
+        self.behavior_names = ['find wall', 'follow wall', 'homing', 'avoid', 'corridor']
         self.lidar = lidar
         self.distance_front = distance_front
         self.distance_left = distance_left
         
+    def check_find_wall(self):
+        # output is dus false als er iets in de buurt is
+        # en anders true
+        pass
     
-    def check_wall_left(self, lidar):
-        # input = lidar
+    def check_wall_left(self):
         # output = is dit aan de hand ja of nee?
-        
-        
-        
-    def check_wall_right(self, lidar):
-        # input = lidar
+        pass
+
+    def check_wall_right(self):
         # output = is dit aan de hand ja of nee?
+        pass
         
-    def 
-    def arbitrate(self, ):
-        for behavior in self.behavior_names:
+    def check_obstacle(self):
+        # output = wil je een obstacle ontwijken ja of nee?
+        pass
+        
+    def check_if_complete(self):
+        # output = wil je terug naar de base ja of nee?
+        pass
+    
+    def check_corridor(self):
+        if self.check_wall_left() and self.check_wall_right():
+            return True
+        
+        return False
+    
+    def getBehavior(control_array):
+        for element in reversed(control_array):
+            if element != 0:
+                return control_array.index(element)
+        
+    def arbitrate(self):
+        control_array = np.zeros(len(self.behavior_names))
+        
+        # find wall behavior
+        control_array[0] = self.check_wall_left()
+        
+        # follow wall behavior
+        control_array[1] = self.check_find_wall()
+        
+        # homing behavior
+        control_array[2] = self.check_wall_left()
+        
+        # corridor behavior
+        control_array[3] = self.check_wall_left()
+        
+        # avoid behaviour
+        control_array[4] = self.check_wall_left()
+        
+        behavior = self.getBehavior(control_array)
+        
+        return behavior
+
+        
+
             
             
         
