708b7cc1b8d0d0bdde9382c1ff2f0d3cb2628d9c
francienbarkhof <fj.barkhof@hotmail.com>
2022-06-20 12:02:19 +0200

    Create corridorDetection.py

---
+++ development/behavior_schema/corridorDetection.py
@@ -0,0 +1,31 @@
+# ROS imports
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+class corridorDetection:
+    def __init__(self):
+        self.detector = "Laser"
+        self.pub = None
+
+    def clbk_laser(self, msg):
+
+        # get distance to the right and left
+        left = min(msg.ranges[213:355])
+        right = min(msg.ranges[781:923])
+
+        self.checkCorridor(left, right)
+    
+    def checkCorridor(self, left, right):
+
+        if (left + right) < 1:
+            return True
+    
+    def detectCorridor(self):
+        rospy.init_node('reading_laser')
+
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+
+        sub = rospy.Subscriber('/m2wr/laser/scan', LaserScan, self.clbk_laser)
+
+        rospy.spin()
