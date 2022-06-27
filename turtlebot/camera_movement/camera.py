293a8c10511a57b3cff2b3822c9c64284e2a8e8d
francienbarkhof <fj.barkhof@hotmail.com>
2022-06-27 19:16:23 +0200

    asd

--- turtlebot/camera_movement/camera.py
+++ turtlebot/camera_movement/camera.py
@@ -3,10 +3,21 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 import time
+import follow_blob
+import warnings
+import rospy
+
+# from turtlebot.camera_movement.main import FollowBlob
+
+fb = follow_blob.FollowBlob()
+warnings.filterwarnings('ignore')
 
 class Camera:
     def __init__(self):
-        self.video = None
+        self.video = 0
+        self.width = 0
+        self.behavior = 2
+        self.centerX = 320
 
     def show_RGB_to_HSV(self, image):
         img_HSV = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
@@ -29,13 +40,15 @@ class Camera:
 
         result = image.copy()
         boxes = np.asarray(boxes)
+        if len(boxes) == 0:
+            return None, None
         left, top = np.min(boxes, axis=0)[:2]
         right, bottom = np.max(boxes, axis=0)[2:]
         cv2.rectangle(result, (left,top), (right,bottom), (255, 0, 0), 2)
         distanceX = (960/(right-left)) * 60
-        centerX = (x+(0.5*w))
+        self.centerX = (x+(0.5*w))
         RGB_result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
-        return RGB_result, centerX
+        return RGB_result, self.centerX
 
     # img = cv2.imread("blob_images/donald.jpg")
     # input, _ = one_big_rect(img)
@@ -60,52 +73,82 @@ class Camera:
     # Onderstaande is de functie is voor blob detection en welke kant hij opgestuurd moet worden
 
     def video_blob_direction(self):
-        # 0 voor ubuntu logitech camera 
+        # 0 voor ubuntu logitech camera (Sien)
         self.video = cv2.VideoCapture(0,cv2.CAP_V4L2)
-        print('video =')
-        print(self.video)
+        # print('thanks')
+        self.width = self.video.get(cv2.CAP_PROP_FRAME_WIDTH)
+        # height = self.video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        # print('width and height')
+        # print(width, height)
 
-
-        # _, frame = self.video.read()
-        # print(f'frame = {frame}')
-        print("1")
         while(self.video.isOpened()):
             print("2")
             # print('in video direction while')
             ret, frame = self.video.read()
+            # print(f'frame shape = {frame.shape}')
             # print(f'frame = {frame}')
             if ret == True:
-                frame, centerX = self.one_big_rect(frame)
-                cv2.imshow('Frame',frame)
+                frame, self.centerX = self.one_big_rect(frame)
+                # print(f'centerX = {centerX}')
+                # print(f'frame shape = {frame.shape}')
+                if frame is None:
+                    print('no orange: turn')
+                    self.behavior = 5
+                    fb.decideBehavior(self.behavior)
+                    continue
+
+                else:
+                    cv2.imshow('Frame',frame)
                 
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     break
+
             else:
-                # print('ret == false')
+                print('ret is false')
                 return None
+            # print(f'width = {self.width}')
+            print(f'centerx = {self.centerX}')
+            print(f'widt = {self.width}')
+            if self.centerX < ((1/5)*self.width):
+                print('cam left')
 
-            if centerX < 460:
-                print('adjustright')
-                return 'adjustright'
-            elif centerX > 500:
-                print('adjustleft')
-                return 'adjustleft'
+                self.behavior = 0
+                # return 0
+            
+            if (self.centerX > (1/5)*self.width) and (self.centerX < (2/5)*self.width):
+                print('cam adjust left')
+                self.behavior = 1
+                # return 1
 
-            else:
-                print('drivestraight')
-                return 'drivestraight'
+            if (self.centerX > (2/5)*self.width) and (self.centerX < (3/5)*self.width):
+                print('cam move forward')
+                self.behavior = 2
+                # return 2
+
+            if (self.centerX > (3/5)*self.width) and (self.centerX < (4/5)*self.width):
+                print('cam adjust right')
+                self.behavior = 3
+                # return 3
+
+            if (self.centerX > ((4/5)*self.width)):
+                print('cam right')
+                self.behavior = 4
+                # return 4
+            
+            fb.decideBehavior(self.behavior)
 
         self.video.release()
 
-        cv2.destroyAllWindows()
+        # cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     # rospy.init_node('FollowBlob', anonymous=False)
     print( " === Starting Program === " )
+    
     # fb = FollowBlob()
     cam = Camera()
     cam.video_blob_direction()
-        # fb.decideBehavior(cam)
+        # fb.decideBehavior(cam)fb
 
 
 
---
+++ turtlebot/camera_movement/follow_blob.py
@@ -0,0 +1,103 @@
+import rospy
+from geometry_msgs.msg import Twist
+# import camera
+import cv2
+
+
+class FollowBlob():
+
+    def __init__(self):
+        rospy.init_node('FollowBlob', anonymous=False)
+        self.pub = rospy.Publisher("/client/cmd_vel", Twist, queue_size=10)
+        msg = Twist()
+        msg.linear = 0.2
+        self.pub.publish(msg)
+
+    def adjust_left(self):
+        self.move(0.1, 0.15, 0.1)
+    
+    def left(self):
+        self.move(0.1, 0.25, 0.1)
+
+    def adjust_right(self):
+        self.move(0.1, -0.15, 0.1)
+
+    def right(self):
+        self.move(0.1, -0.25, 0.1)
+
+    def move_forward(self):
+        self.move(0.1, 0.0, 0.1)
+    
+    def turn(self):
+        self.move(0, -0.5, 0.1)
+    
+    def decideBehavior(self, behavior):
+        # print(f'video blob direction {cam.video_blob_direction()}' )
+        # print(f'video blob direction {type(cam.video_blob_direction())}' )
+        
+        if behavior == 0:
+            print(('left'), 0.1)
+    
+    def decideBehavior(self, behavior):
+        # print(f'video blob direction {cam.video_blob_direction()}' )
+        # print(f'video blob direction {type(cam.video_blob_direction())}' )
+        
+        if behavior == 0:
+            print('left')
+            self.left()
+        
+        elif behavior == 1:
+            print('adjust left')
+            self.adjust_left()
+        
+        elif behavior == 2:
+            print('move forward')
+            self.move_forward()
+
+        elif behavior == 3:
+            print('adjust right')
+            self.adjust_right()
+
+        elif behavior == 4:
+            print('right')
+            self.right()
+        
+        elif behavior == 5:
+            self.turn()
+
+        # if cam.video_blob_direction() == 0:
+        #     print('left')
+        #     self.left()
+
+        # elif cam.video_blob_direction() == 1:
+        #     print('adjust left')
+        #     self.adjust_left()
+
+        # elif cam.video_blob_direction() == 2:
+        #     print('move forward')
+        #     self.move_forward()
+
+        # elif cam.video_blob_direction() == 3:
+        #     print('adjust right')
+        #     self.adjust_right()
+
+        # elif cam.video_blob_direction() == 4:
+        #     print('right')
+        #     self.right()
+
+        # elif cam.video_blob_direction() == 5:
+        #     print('turn')
+        #     self.turn()
+
+        else:
+            print(' no action')
+            return None
+
+    def move(self, lin, ang, dur):
+        print('in move')
+        msg = Twist()
+        msg.linear.x = lin
+        msg.angular.z = ang
+        self.pub.publish(msg)
+        # print(f'msg = {msg}')
+        rospy.sleep(dur)
